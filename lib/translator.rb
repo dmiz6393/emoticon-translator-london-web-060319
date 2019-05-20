3d4c13fb8cd7959dea6d34ef43ae6dbf88a7929e
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-20 06:52:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library
  
  emoticons.each do |word,faces| 
  emoticons_hash["emoticon][faces.first]=faces.last 
- emoticons_hash["meaning]
+ emoticons_hash["meaning][faces.last]
 end
 
 def get_japanese_emoticon
